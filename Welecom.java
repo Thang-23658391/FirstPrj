580dce10d3f6be84d8a6a02a642d1c6bc17f9cb1
Thang-23658391 <23658391.thang@student.iuh.edu.vn>
2024-08-13 17:02:07 +0700

    thang

---
+++ Welecom.java
@@ -0,0 +1,6 @@
+public class Welecom {
+    public static void main(String[] args) {
+        System.out.println("heloo Thang");
+        System.out.println("welcome to DHKTPM19ATT-422000411904");
+    }
+}
---
+++ src/welcome.java
@@ -0,0 +1,6 @@
+public class welcome {
+    public static void main(String[] args) {
+        System.out.println("heloo Thang");
+        System.out.println("welcome to DHKTPM19ATT-422000411904");
+    }
+}
